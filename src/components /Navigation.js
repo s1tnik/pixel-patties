262d90f99712dc9d9cff7ac802af662716d74e31
s1tnikk <viacheslav@spotnana.com>
2022-05-11 14:02:17 +0300

    update navigation

--- src/components /Navigation.js
+++ src/components /Navigation.js
@@ -1,40 +1,53 @@
-import { useCallback, useEffect, useRef } from "react";
-import scrollTo from "../helpers/scrollTo";
-const Navigation = ({ screens, children }) => {
-  const currentScreen = useRef(0);
-  const lastWheelCall = useRef(null);
+import {useCallback, useEffect, useRef} from "react";
 
-  const scrollNavigation = useCallback(
-    ({ deltaY }) => {
-      if (
-        lastWheelCall.current === null ||
-        lastWheelCall.current + 500 - Date.now() < 0
-      ) {
-        lastWheelCall.current = Date.now();
+const Navigation = ({screens, children}) => {
+    const currentScreen = useRef(0);
+    const lastWheelCall = useRef(null);
 
-        if (deltaY > 0 && currentScreen.current < screens.length - 1) {
-          scrollTo(screens[currentScreen.current + 1]);
-          currentScreen.current = currentScreen.current + 1;
+    const scrollNavigation = useCallback(
+        ({deltaY}) => {
+            if (
+                lastWheelCall.current === null ||
+                lastWheelCall.current + 500 - Date.now() < 0
+            ) {
+                lastWheelCall.current = Date.now();
+
+                if (deltaY > 0 && currentScreen.current < screens.length - 1) {
+                    window.document.querySelector(".scroll-navigation").scrollBy({
+                        top: 0,
+                        left: window.innerWidth,
+                        behavior: 'smooth'
+                    })
+                    currentScreen.current = currentScreen.current + 1;
+                }
+
+                if (deltaY < 0 && currentScreen.current > 0) {
+                    window.document.querySelector(".scroll-navigation").scrollBy({
+                        top: 0,
+                        left: -window.innerWidth,
+                        behavior: 'smooth'
+                    })
+                    currentScreen.current = currentScreen.current - 1;
+                }
+            }
+        },
+        [screens]
+    );
+
+    useEffect(() => {
+        if (screens.every(({current}) => current)) {
+            window.document.addEventListener("wheel", scrollNavigation);
         }
 
-        if (deltaY < 0 && currentScreen.current > 0) {
-          scrollTo(screens[currentScreen.current - 1]);
-          currentScreen.current = currentScreen.current - 1;
-        }
-      }
-    },
-    [currentScreen.current, screens]
-  );
+        return () => window.document.removeEventListener("wheel", scrollNavigation);
+    }, []);
 
-  useEffect(() => {
-    if (screens.every(({ current }) => current)) {
-      window.document.addEventListener("wheel", scrollNavigation);
-    }
 
-    return () => window.document.removeEventListener("wheel", scrollNavigation);
-  }, []);
-
-  return <>{children}</>;
+    return (
+        <div className="scroll-navigation">
+            <div className="scroll-wrapper">{children}</div>
+        </div>
+    );
 };
 
 export default Navigation;
--- src/helpers/scrollTo.js
+++ src/helpers/scrollTo.js
@@ -1,7 +1,11 @@
 const scrollTo = (ref) => {
-  if (ref && ref.current /* + other conditions */) {
-    ref.current.scrollIntoView({ behavior: "smooth", block: "center" });
-  }
+    if (ref && ref.current /* + other conditions */) {
+        ref.current.scrollIntoView({
+            behavior: 'smooth',
+            block: 'center',
+            inline: 'center'
+        });
+    }
 };
 
 export default scrollTo;
